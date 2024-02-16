687f2fdb081956f54358f29b08b7015ae4faa614
Davron <tdavron@gmail.com>
2024-02-16 11:37:30 +0500

    Add project files.

---
+++ GetHardwareID/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var dl = new Dictionary<string, string>() {
+            { "LinuxModel", "cat /sys/class/dmi/id/product_name" },
+            { "LinuxModelVersion", "cat /sys/class/dmi/id/product_version" },
+            { "LinuxFirmwareVersion", "cat /sys/class/dmi/id/bios_version" },
+            { "LinuxMachineId", "cat /var/lib/dbus/machine-id" }
+        };
+
+        foreach (var it in dl)
+        {
+            Console.WriteLine($"{it.Key}: {Bash(it.Value)}");
+        }
+
+    }
+
+    public static string Bash(string cmd)
+    {
+        string result = String.Empty;
+
+        try
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+
+                process.Start();
+                result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit(1500);
+                process.Kill();
+            };
+        }
+        catch (Exception ex)
+        {
+            //Logger.ErrorFormat(ex.Message, ex);
+        }
+        return result;
+    }
+}
